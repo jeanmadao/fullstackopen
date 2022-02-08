f6ee150aba96d6af24d5503427db3bec95cb45b0
Jean Pierre Huynh <huynhjeanpierre@hotmail.com>
2022-02-08 03:11:28 +0100

    1.8-9: unicafe step3-4 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -9,6 +9,11 @@ const Statistics = ({ feedbacks }) => {
   const all = good + neutral + bad
   const average = (good - bad) / all
   const positive = good / all * 100
+  if (all === 0) {
+    return (
+      <div>No feedback given</div>
+    )
+  }
   return (
     <div>
       <div>good {good}</div>
