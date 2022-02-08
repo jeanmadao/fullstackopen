80599f94e74cbaf994aa3634bd47fa76646e0b50
Jean Pierre Huynh <huynhjeanpierre@hotmail.com>
2022-02-08 19:13:35 +0100

    2.1: course information step6 done

---
+++ part2/courseinfo/src/components/Course.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const Header = ({ course }) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Total = ({ course }) => {
+  const sum = course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises
+  return (
+    <p>Number of exercises {sum}</p>
+  )
+}
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <div>
+      {course.parts.map(part =>
+        <Part key={part.id} part={part} />
+      )}
+    </div>
+  )
+}
+
+const Course = ({ course }) => {
+  return (
+    <div>
+      <Header course={course} />
+      <Content course={course} />
+      {/*
+      <Total course={course} />
+      */}
+    </div>
+  )
+}
+
+export default Course
