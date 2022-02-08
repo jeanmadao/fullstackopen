5bb978e573a9328e5e9622c3c58f6afad9e53f93
Jean Pierre Huynh <huynhjeanpierre@hotmail.com>
2022-02-08 03:06:12 +0100

    1.7: unicafe step2 done

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -6,11 +6,17 @@ const Button = ({ text, handleClick }) => (
 
 const Statistics = ({ feedbacks }) => {
   const [good, neutral, bad] = feedbacks
+  const all = good + neutral + bad
+  const average = (good - bad) / all
+  const positive = good / all * 100
   return (
     <div>
       <div>good {good}</div>
       <div>neutral {neutral}</div>
       <div>bad {bad}</div>
+      <div>all {all}</div>
+      <div>average {average}</div>
+      <div>positive {positive} %</div>
     </div>
   )
 }
