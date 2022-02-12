c35fe2d4df3aabf8e10c82c7af655c73bd9f4db5
Jean Pierre Huynh <huynhjeanpierre@hotmail.com>
2022-02-12 01:33:26 +0100

    2.9: The Phonebook Step4 done

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -3,13 +3,18 @@ import Person from './components/Person'
 
 const App = () => {
   const [persons, setPersons] = useState([
-    {
-      name: 'Arto Hellas',
-      number: '040-1234567',
-    }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ])
+  const [filter, setFilter] = useState('')
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
+
+  const handleFilterChange = (event) => {
+    setFilter(event.target.value)
+  }
 
   const handleNameChange = (event) => {
     setNewName(event.target.value)
@@ -24,6 +29,7 @@ const App = () => {
     const personObject = {
       name: newName,
       number: newNumber,
+      id: persons.length,
     }
     if (persons.find(person => person.name === newName)) {
       window.alert(`${newName} is already added to phonebook`)
@@ -34,9 +40,19 @@ const App = () => {
     }
   }
 
+  const personsToShow = persons.filter(person => person.name.toLowerCase().includes(filter.toLowerCase()))
+
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>filter shown with
+        <input
+          value={filter}
+          onChange={handleFilterChange}
+        />
+      </div>
+
+      <h2>add a new</h2>
       <form>
         <div>name:
           <input
@@ -59,9 +75,10 @@ const App = () => {
           </button>
         </div>
       </form>
+
       <h2>Numbers</h2>
       <ul>
-        {persons.map(person =>
+        {personsToShow.map(person =>
           <Person key={person.name} person={person} />
         )}
       </ul>
