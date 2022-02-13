54451586c879cb1c0a1fa9b6da219f1198bab312
Jean Pierre Huynh <huynhjeanpierre@hotmail.com>
2022-02-13 04:17:01 +0100

    2.14: Data for countries, step3 done

--- part2/countries/src/components/Country.js
+++ part2/countries/src/components/Country.js
@@ -1,4 +1,5 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Country = ({ country, solo }) => {
   const [show, setShow] = useState(false)
@@ -10,6 +11,22 @@ const Country = ({ country, solo }) => {
   if (solo && show === false) {
     setShow(true)
   }
+
+  useEffect(() => {
+    axios
+      .get(`https://api.openweathermap.org/data/2.5/weather?lat=${country.capitalInfo.latlng[0]}&lon=${country.capitalInfo.latlng[1]}&appid=${process.env.REACT_APP_API_KEY}`)
+      .then(response => {
+        const weatherJson = JSON.parse(response.request.response)
+        const temperatureDiv = document.getElementById('temperature')
+        const weatherImg = document.getElementById('weather-img')
+        const windDiv = document.getElementById('wind')
+
+        temperatureDiv.innerText = `temperature ${Math.round((weatherJson.main.temp - 273.15 + Number.EPSILON) * 100) / 100} Celcius`
+        weatherImg.setAttribute("src", `http://openweathermap.org/img/wn/${weatherJson.weather[0].icon}@2x.png`)
+        windDiv.innerText = `wind ${weatherJson.wind.speed} m/s`
+      })
+  })
+
 
   if (show) {
     return (
@@ -26,6 +43,10 @@ const Country = ({ country, solo }) => {
           )}
         </ul>
         <img src={country.flags.png} alt={`${country.name.common} flag`} />
+        <h2>Weather in {country.capital}</h2>
+        <div id="temperature"></div>
+        <img id="weather-img" />
+        <div id="wind"></div>
       </div>
     )
   } else {
