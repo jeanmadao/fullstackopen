13458b8715cef5a4abc994b20cf648edeb465d9d
Jean Huynh <huynhjeanpierre@hotmail.com>
2023-07-18 23:34:16 +0200

    2.2: Course information step7 & 2.3*: Course information step8

--- part2/courseinfo/src/components/Course.js
+++ part2/courseinfo/src/components/Course.js
@@ -1,11 +1,14 @@
 import Header from './Header'
 import Content from './Content'
+import Total from './Total'
 
 const Course = ({ course }) => {
+  const total = course.parts.reduce((s, p) => s + p.exercises, 0)
   return(
     <div>
       <Header name={course.name} />
       <Content parts={course.parts} />
+      <Total total={total} />
     </div>
   )
 }
---
+++ part2/courseinfo/src/components/Total.js
@@ -0,0 +1,9 @@
+const Total = ({ total }) => {
+  return (
+    <b>
+      total of {total} exercises
+    </b>
+  )
+}
+
+export default Total
