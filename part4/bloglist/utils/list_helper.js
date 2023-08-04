12268383f4cd93794102454190f08a7cc425d957
Jean Huynh <huynhjeanpierre@hotmail.com>
2023-08-04 15:53:21 +0200

    mostBlog function created

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,8 +1,22 @@
+const _ = require('lodash')
+
 const totalLikes = blogs => blogs.reduce((acc, curr) => curr.likes + acc, 0)
 
-const favoriteBlog = blogs => blogs.reduce((max, curr) => (!max || curr.likes > max.likes) ? curr : max, null)
+const favoriteBlog = blogs => blogs.reduce((max, curr) => (!max || curr.likes > max.likes) ? _.pick(curr, ['title', 'author', 'likes']) : max, null)
+
+// const mostBlogs = blogs => _.transform(_.countBy(blogs, 'author'), (max, val, key) => (!max || val > max.blogs) ? { author: key, blogs: val } : max, null)
+
+const mostBlogs = blogs => _.transform(_.countBy(blogs, 'author'), (max, val, key) => {
+  if (val > max.blogs) {
+    max.author = key
+    max.blogs = val
+  }
+}, { author: null, blogs: 0 })
+
+
 
 module.exports = {
   totalLikes,
-  favoriteBlog
+  favoriteBlog,
+  mostBlogs
 }
