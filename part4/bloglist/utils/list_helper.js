9fafedc0634ef1c55c57054764d17574fff05176
Jean Huynh <huynhjeanpierre@hotmail.com>
2023-08-04 02:14:25 +0200

    .5*: helper functions and unit tests, step3

--- part4/bloglist/utils/list_helper.js
+++ part4/bloglist/utils/list_helper.js
@@ -1,5 +1,8 @@
 const totalLikes = blogs => blogs.reduce((acc, curr) => curr.likes + acc, 0)
 
+const favoriteBlog = blogs => blogs.reduce((max, curr) => (!max || curr.likes > max.likes) ? curr : max, null)
+
 module.exports = {
-  totalLikes
+  totalLikes,
+  favoriteBlog
 }
